c06371c129aadc1d0cc6d3ef1cb781920694c08c
manuel-naranjo-blink <manuel.naranjo@blinklearning.com>
2024-04-01 11:55:43 +0200

    libA to default

--- src/libs/lib-a/index.jsx
+++ src/libs/lib-a/index.jsx
@@ -1,10 +1,9 @@
-import React from "react";
 import InputNumber from "./components/InputNumber/index.jsx";
 import Button from "./components/Button/index.jsx";
 import AppBar from "./components/AppBar";
 
 export default {
     Button: Button,
-    InputNumber: InputNumber,
-    AppBar: AppBar
+    AppBar: AppBar,
+    InputNumber: InputNumber
 }
--- src/libs/lib-b/index.jsx
+++ src/libs/lib-b/index.jsx
@@ -2,10 +2,10 @@ import CustomText from "./components/CustomText";
 import Button from "./components/Button";
 import AppBar from "./components/AppBar";
 import libA from "../lib-a/index.jsx";
+
 export default {
     ...libA,
     Button: Button,
-    CustomText: CustomText,
-    AppBar: AppBar
+    AppBar: AppBar,
+    CustomText: CustomText
 }
-
